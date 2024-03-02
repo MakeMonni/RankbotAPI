5b7c45927795c371c4da22672d31bfd37b64fba4
MakeMonni <makemake.nylund@gmail.com>
2024-03-03 01:55:41 +0200

    remove logging

--- routes/get/deleted.js
+++ routes/get/deleted.js
@@ -1,7 +1,6 @@
 const deleted = async (ctx) => {
 
     const maps = await ctx.db.collection("beatSaverLocal").find({ deleted: true }).project({"versions.hash": 1}).toArray();
-    console.log(maps)
     const playlistHashes = await ctx.helpers.hashes(maps);
     const playlist = await ctx.helpers.createPlaylist("Lost & forgotten maps", playlistHashes, ``, `deleted`, "This playlist contains maps that are deleted.")
     ctx.body = playlist;
